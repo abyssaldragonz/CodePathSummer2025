1f05fc6b733081f08ac75bb7c1fa54f4d62983c7
Josephine <jbl.noetic@gmail.com>
2025-08-24 14:59:12 -0500

    lab five

---
+++ labs/5-cap/src/App.jsx
@@ -0,0 +1,32 @@
+import { useState } from 'react'
+import './App.css'
+import APIForm from './components/APIForm';
+const ACCESS_KEY = import.meta.env.VITE_APP_ACCESS_KEY;
+
+function submitForm() {
+ 
+
+}
+
+function App() {
+  return (
+    <div className="whole-page">
+      <h1>Build Your Own Screenshot! 📸</h1>
+      
+      <APIForm
+        inputs={inputs}
+        handleChange={(e) =>
+          setInputs((prevState) => ({
+            ...prevState,
+            [e.target.name]: e.target.value.trim(),
+          }))
+        }
+        onSubmit={submitForm}
+      />
+      <br></br>
+
+    </div>
+  );
+}
+
+export default App
